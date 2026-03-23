d2fbd368fcd3ce7a9b690cd0076bd24577793cb7
mgerpelandeira <mgerpelandeira@gmail.com>
2026-03-23 11:11:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("10PaMates iniciado correctamente");
+function comprobarRespuesta(correcto) {
+    if (correcto) {
+        alert("Correcto ✅");
+    } else {
+        alert("Incorrecto ❌");
+    }
+}
