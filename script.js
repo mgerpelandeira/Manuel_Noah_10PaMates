952fd46b2c6762322974748742772cfaf098f4d2
mgerpelandeira <mgerpelandeira@gmail.com>
2026-04-17 11:03:11 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
-function comprobarRespuesta(correcto) {
-    let resultado = document.getElementById("resultado");
+let puntuacion = 0;
+let preguntasRespondidas = 0;
 
-    if (correcto) {
-        resultado.innerHTML = "✅ Correcto";
+function responder(correcta) {
+    preguntasRespondidas++;
+
+    if (correcta) {
+        puntuacion++;
+        alert("✅ Correcto");
     } else {
-        resultado.innerHTML = "❌ Incorrecto";
+        alert("❌ Incorrecto");
     }
+
+    // Si ya respondió las 10
+    if (preguntasRespondidas === 10) {
+        mostrarNota();
+    }
+}
+
+function mostrarNota() {
+    let nota = (puntuacion / 10) * 10;
+
+    let mensaje = "";
+
+    if (nota >= 9) {
+        mensaje = "🔥 Sobresaliente";
+    } else if (nota >= 7) {
+        mensaje = "👏 Notable";
+    } else if (nota >= 5) {
+        mensaje = "👍 Aprobado";
+    } else {
+        mensaje = "❌ Suspenso";
+    }
+
+    document.getElementById("resultado").innerHTML =
+        "Puntuación: " + puntuacion + "/10<br>Nota: " + nota + " - " + mensaje;
 }
